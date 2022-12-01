91cb295b5ea4acd8d9cf8f547acee0951a25c515
Kyle Cuss <cussprogramming@gmail.com>
2022-12-01 08:45:51 -0330

    added particle effects and sound effects/music

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,28 +9,37 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier; //variable adjust level of gravity
     public bool isOnGround = true; //variable checks is player is on the ground
     public bool gameOver = false; //gameover true or false
+    public ParticleSystem explosionParticle; //variable to set particle type
+    public ParticleSystem dirtParticle; //variable to set particle type
+    public AudioClip jumpSound; //variable to set audio clips
+    public AudioClip crashSound; //variable to set audio clips
 
     //private variables
     private Rigidbody playerRB; //variable for rigidbody component
-    private Animator playerAnim; //variable for animator
+    private Animator playerAnim; //variable for animator component
+    private AudioSource playerAudio; //variable for audiosource component
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>(); //gets rigidbody component from player object
-        Physics.gravity *= gravityModifier; //physics for gravity is equal to gravity * gravitiyModifier
         playerAnim = GetComponent<Animator>(); //gets animator component
+        playerAudio = GetComponent<AudioSource>(); //get Audio Source component
+        Physics.gravity *= gravityModifier; //physics for gravity is equal to gravity * gravitiyModifier
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //player presses spacebar and on ground will jump
+        //player presses spacebar AND on ground will jump AND game not over
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //enacts physics on rigidbody component, impulse enacts force immediately rather than over time
             isOnGround = false; //player not on ground 
             playerAnim.SetTrigger("Jump_trig"); //activates jump animation via jump trigger in animation tree
+            dirtParticle.Stop(); //stops particle effect
+            playerAudio.PlayOneShot(jumpSound, 1.0f); //play sound effect once at full volume
         }
     }
 
@@ -41,6 +50,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true; //changes isOnGround to true
+            dirtParticle.Play(); //plays particle effect
         } 
         //if player collides with obstacle game over
         else if (collision.gameObject.CompareTag("Obstacle"))
@@ -49,6 +59,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over"); //logs Game Over to console
             playerAnim.SetBool("Death_b", true); //activates death animation
             playerAnim.SetInteger("DeathType_int", 1); //sets animation in animation tree
+            explosionParticle.Play(); //plays particle effect
+            dirtParticle.Stop(); //stops particle effect
+            playerAudio.PlayOneShot(crashSound, 1.0f); //play sound effect once at full volume
         }
     }
 }
