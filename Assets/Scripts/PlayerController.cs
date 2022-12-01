b6cc74219e3e422694bcf3611986ce35c3d291dc
Kyle Cuss <cussprogramming@gmail.com>
2022-12-01 08:08:35 -0330

    added player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //public variables
     public float jumpForce = 10; //variable how high jump is
     public float gravityModifier; //variable adjust level of gravity
     public bool isOnGround = true; //variable checks is player is on the ground
     public bool gameOver = false; //gameover true or false
 
+    //private variables
     private Rigidbody playerRB; //variable for rigidbody component
+    private Animator playerAnim; //variable for animator
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>(); //gets rigidbody component from player object
         Physics.gravity *= gravityModifier; //physics for gravity is equal to gravity * gravitiyModifier
+        playerAnim = GetComponent<Animator>(); //gets animator component
     }
 
     // Update is called once per frame
     void Update()
     {
         //player presses spacebar and on ground will jump
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); //enacts physics on rigidbody component, impulse enacts force immediately rather than over time
             isOnGround = false; //player not on ground 
+            playerAnim.SetTrigger("Jump_trig"); //activates jump animation via jump trigger in animation tree
         }
     }
 
@@ -42,6 +47,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;//changes gameOver to true
             Debug.Log("Game Over"); //logs Game Over to console
+            playerAnim.SetBool("Death_b", true); //activates death animation
+            playerAnim.SetInteger("DeathType_int", 1); //sets animation in animation tree
         }
     }
 }
